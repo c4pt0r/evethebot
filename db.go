89b8654123bc970d2649934840412d2e3b514d31
dongxu <i@huangdx.net>
2021-09-23 17:45:16 +0800

    backup

--- db.go
+++ db.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"flag"
+	"log"
+	"sync"
 	"time"
 
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
 var (
@@ -12,8 +16,36 @@ var (
 )
 
 type SessionModel struct {
-	ChatID   int64
+	gorm.Model
+
+	ChatID   int64 ``
 	Token    string
 	From     string
 	CreateAt time.Time
+}
+
+var (
+	_once_db   sync.Once
+	_global_db *gorm.DB
+)
+
+func init() {
+	// FIXME
+	DB()
+}
+
+func DB() *gorm.DB {
+	_once_db.Do(func() {
+		var err error
+		_global_db, err = gorm.Open(sqlite.Open(*dbPath), &gorm.Config{})
+		if err != nil {
+			log.Fatal(err)
+		}
+		_global_db.AutoMigrate(&SessionModel{})
+	})
+	return _global_db
+}
+
+func PutOrUpdate(m *SessionModel) error {
+	return nil
 }
--- go.mod
+++ go.mod
@@ -8,4 +8,6 @@ require (
 	github.com/google/uuid v1.3.0
 	github.com/jinzhu/gorm v1.9.16
 	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
+	gorm.io/driver/sqlite v1.1.5
+	gorm.io/gorm v1.21.15
 )
